775a529a98ce6c0561e771a59f9bb6897f8557d7
Daniel Ndashimye <50277825+Dahdo@users.noreply.github.com>
2026-03-31 18:19:59 +0200

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {  
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) // an empty tree is always a subtree
+            return true;
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (isSameTree(node, subRoot))
+                return true;
+            if (node != null) {
+                q.offer(node.left);
+                q.offer(node.right);
+            }
+        }
+
+        return false;
+    }
+
+    boolean isSameTree(TreeNode p, TreeNode q) {
+        // my previous solution for Same Tree problem
+
+        Queue<TreeNode> pQ = new LinkedList<>();
+        Queue<TreeNode> qQ = new LinkedList<>();
+
+        pQ.offer(p);
+        qQ.offer(q);
+
+        while (!pQ.isEmpty() || !qQ.isEmpty()) {
+            TreeNode pNode = pQ.poll();
+            TreeNode qNode = qQ.poll();
+
+            if (pNode == null && qNode == null)
+                continue;
+            else if (!(pNode != null && qNode != null))
+                return false;
+            else {
+                if (pNode.val != qNode.val)
+                    return false;
+                pQ.offer(pNode.left);
+                pQ.offer(pNode.right);
+                qQ.offer(qNode.left);
+                qQ.offer(qNode.right);
+            }
+        }
+
+        return true;
+    }
+}
